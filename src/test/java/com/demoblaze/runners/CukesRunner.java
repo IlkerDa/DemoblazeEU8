55b607e47fd2a65662da6111ff4813f7e799e4d3
ilker <ilkerdagdeviren@yahoo.com>
2022-06-30 15:09:57 +0300

    2nd Commit

--- src/test/java/com/demoblaze/runners/CukesRunner.java
+++ src/test/java/com/demoblaze/runners/CukesRunner.java
@@ -1,23 +1,20 @@
 package com.demoblaze.runners;
 
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-@RunWith((Cucumber.class))
+@RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
-                "html:target/cucumber-report.html"
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                "json:target/cucumber.json"
         },
-        features = "src/test/resources/features",
-        glue = "src/test/java/com/demoblaze/step_definitions",
-        dryRun = true,
+        features ="src/test/resources/features",
+        glue = "com/demoblaze/step_definitions",
+        dryRun = false,
         tags = "@wip"
-
-
-
 )
 public class CukesRunner {
-
 }
